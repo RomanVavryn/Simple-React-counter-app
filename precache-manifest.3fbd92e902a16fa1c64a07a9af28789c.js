95bf7cba1a7cebab9fd78eb1e0dda92b2f8f1f69
Roman Vavtunkevuch <roman.vavrun@gmail.com>
2019-09-17 22:24:30 +0300

    Updates

---
+++ precache-manifest.3fbd92e902a16fa1c64a07a9af28789c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8219b2b6a12a0b0841b2cbf59f047131",
+    "url": "/Simple-React-counter-app/index.html"
+  },
+  {
+    "revision": "75e8ff72ce60ab2ec221",
+    "url": "/Simple-React-counter-app/static/css/main.da57bf96.chunk.css"
+  },
+  {
+    "revision": "bd224ffc6d68cf200660",
+    "url": "/Simple-React-counter-app/static/js/2.eef472e2.chunk.js"
+  },
+  {
+    "revision": "75e8ff72ce60ab2ec221",
+    "url": "/Simple-React-counter-app/static/js/main.516d1c17.chunk.js"
+  },
+  {
+    "revision": "a785271c729ecc721a1a",
+    "url": "/Simple-React-counter-app/static/js/runtime~main.8fb81e4e.js"
+  }
+]);
